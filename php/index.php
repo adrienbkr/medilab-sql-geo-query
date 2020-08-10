b34ae86b065b73fc063dd2120bd875464c8cdc3f
Luc Bernard <lubernar@student.42.fr>
2020-08-10 15:31:53 +0200

    feat: big join

--- php/index.php
+++ php/index.php
@@ -17,23 +17,108 @@ try {
   $db = new PDO("mysql:host=$host;port=$port;dbname=$database", $user, $pass);
 
   // // MODEL
-  // $model = $db->prepare("CREATE TABLE scans (id INT PRIMARY KEY NOT NULL AUTO_INCREMENT, point POINT)");
-  // $model->execute();
+  $model = $db->prepare("CREATE TABLE scans (
+    id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    point POINT,
+    userId INT,
+    companyId INT,
+    itemId INT,
+    createdAt DATE,
+    legit INT
+  )");
+  $model->execute();
+
+  // $company = $db->prepare("CREATE TABLE company (
+  //   id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  //   name VARCHAR( 10 )
+  // )");
+  // $company->execute();
+
+  // $item = $db->prepare("CREATE TABLE item (
+  //   id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  //   name VARCHAR( 10 )
+  // )");
+  // $item->execute();
+
+  // $users = $db->prepare("CREATE TABLE users (
+  //   id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  //   name VARCHAR( 10 ),
+  //   type VARCHAR( 10 )
+  // )");
+  // $users->execute();
+
 
   // // WRITE
-  // $write = $db->prepare("INSERT INTO scans (point) VALUES (POINT(:lat, :lng))");
+  // $start = strtotime("10 September 2000");
+  // $end = strtotime("22 July 2010");
+  // $write = $db->prepare("INSERT INTO scans (point, userId, companyId, itemId, createdAt, legit) VALUES (POINT(:lat, :lng), :userId, :companyId, :itemId, :createdAt, :legit)");
   // $write->bindParam(":lat", $lat);
   // $write->bindParam(":lng", $lng);
+  // $write->bindParam(":userId", $userId);
+  // $write->bindParam(":companyId", $companyId);
+  // $write->bindParam(":itemId", $itemId);
+  // $write->bindParam(":legit", $legit);
+  // $write->bindParam(":createdAt", $createdAt);
   // for ($i = 0; $i < 50000; $i++) {
   //   $lat = 44 + round(-5 * 1000 + rand(0, 10 * 1000)) / 1000;
   //   $lng = 0 + round(-5 * 1000 + rand(0, 10 * 1000)) / 1000;
+  //   $userId = rand(1, 2);
+  //   $companyId = rand(1, 2);
+  //   $itemId = rand(1,3);
+  //   $legit = rand(0,1);
+  //   $timestamp = mt_rand($start, $end);
+  //   $createdAt = date("Y-m-d", $timestamp);
   //   $write->execute();
+  // }
+  // $name = "Biogaran";
+  // $type = "user";
+  // $write = $db->prepare("INSERT INTO company (name) VALUES (:name)");
+  // $write->bindParam(":name", $name);
+  // $write->bindParam(":type", $type);
+  // for ($i = 0; $i < 50000; $i++) {
+  //   $lat = 44 + round(-5 * 1000 + rand(0, 10 * 1000)) / 1000;
+  //   $lng = 0 + round(-5 * 1000 + rand(0, 10 * 1000)) / 1000;
+    // $write->execute();
   // }
 
   // READ
   // $read = $db->prepare("SELECT ST_AsGeojson(point) AS geojson, ST_Distance(point, POINT(0, 0)) AS dist_meter from scans");
-  $read = $db->prepare("SELECT ST_AsGeojson(point) AS geojson, ROUND(ST_Distance_Sphere(point, POINT(45, 0))) / 1000 AS dist FROM scans WHERE ST_CONTAINS(ST_GEOMFROMTEXT('POLYGON((44 -1, 44 1, 46 1, 44 -1))'), point) ORDER BY dist");
+  $read = $db->prepare("
+    SELECT
+    ST_AsGeojson(point) AS geojson,
+    ROUND(ST_Distance_Sphere(point, POINT(45, 0))) / 1000 AS dist
+    FROM scans
+    LEFT JOIN company ON scans.companyId = company.id
+    LEFT JOIN users ON scans.userId = users.id
+    LEFT JOIN item ON scans.itemId = item.id
+    WHERE ST_CONTAINS(ST_GEOMFROMTEXT('POLYGON((44 -1, 44 1, 46 1, 44 -1))'), point)
+    AND company.name = :company_name
+      AND users.type = :user_type
+      AND item.name = :item_name
+      AND scans.createdAt BETWEEN :from AND :to
+    ORDER BY dist");
+  // $read = $db->prepare("SELECT ST_AsGeojson(point) AS geojson, ROUND(ST_Distance_Sphere(point, POINT(45, 0))) / 1000 AS dist FROM scans WHERE ST_CONTAINS(ST_GEOMFROMTEXT('POLYGON((44 -1, 44 1, 46 1, 44 -1))'), point) ORDER BY dist");
   // $read = $db->prepare("SELECT ST_AsGeoJSON(point) as geojson from scans");
+  $company_name = "UPSA";
+  $user_type = "user";
+  $item_name = "Efferalgan";
+  $from = "2005-04-20";
+  $to = "2010-04-20";
+   if (isset($_GET["company_name"]))
+    $company_name = $_GET["company_name"];
+  if (isset($_GET["user_type"]))
+    $user_type = $_GET["user_type"];
+  if (isset($_GET["item_name"]))
+    $item_name = $_GET["item_name"];
+  if (isset($_GET["from"]))
+    $from = $_GET["from"];
+  if (isset($_GET["to"]))
+    $to = $_GET["to"];
+  $read->bindParam(":company_name", $company_name);
+  $read->bindParam(":user_type", $user_type);
+  $read->bindParam(":item_name", $item_name);
+  $read->bindParam(":from", $from);
+  $read->bindParam(":to", $to);
   $read->execute();
   $rows = $read->fetchAll(PDO::FETCH_OBJ);
   // echo json_encode($rows);
@@ -68,7 +153,12 @@ try {
       [44, 1],
       [46, 1]
     ]).addTo(mymap);
-    
+
+    var markers = L.markerClusterGroup();
+    markers.addLayer(L.marker(getRandomLatLng(mymap)));
+    mymap.addLayer(markers);
+
+
     markers.forEach(marker => {
       L.marker(JSON.parse(marker.geojson).coordinates).addTo(mymap)
     })
